e8779d5b9a57f9787391caeb79082f72ae138b92
Matt Goodwin <matt.goodwin491@gmail.com>
2014-02-23 15:15:38 +0000

    Fix #1 - Mediator should implement IMediator

--- lib/ServiceBus/IMediator.php
+++ lib/ServiceBus/IMediator.php
@@ -9,12 +9,12 @@ interface IMediator
      *
      * @param IQuery
      */
-    public function Request(IQuery $query);
+    public function request(IQuery $query);
     
     /**
      * Send a command into a deep hole expecting no return 
      *
      * @param ICommand
      */
-    public function Send(ICommand $query);
+    public function send(ICommand $query);
 }
